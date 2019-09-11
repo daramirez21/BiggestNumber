730c0a0040ea88b60fd706791ac977fcd7f63976
daramirez21 <daramirez@mail.csuchico.edu>
2019-09-11 16:37:37 -0700

    Gets the biggest number of two whole numbers

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,20 +1,33 @@
-//Author:
+//Author: David Ramirez
 #include<iostream>
+
+
+using std::cin;
+using std::cout;
+using std::endl;
 
 int main()
 {
-  //declare variables here
+  int first = 0;
+  int second = 0;
 
-  std::cout<<"Please enter a whole number:\n";
-  //get first input using std::cin>>
+  std::cout<<"Please enter a whole number: ";
+  cin>>first;
 
-  std::cout<<"Please enter another whole number:\n";
+  std::cout<<"Please enter another whole number: ";
+  cin>>second;
+
   //get second input using std::cin>> again with a second variable
 
+  if (first > second)  {
+          std::cout<< "Of those two numbers biggest number is  " << first << endl;
+  }
+  else if (second > first) {
+          std::cout<< "Of those two numbers, biggest number is  " << second << endl;
+  }
   //determine if first or second is bigger and store that in a third variable
 
 
-  std::cout<<"Of those two numbers, the biggest is: ";
   //print out the variable representing the biggest number
 
   std::cout<<std::endl<<"Thank you for playing.\n";
